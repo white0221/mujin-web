826812cb422d4e1f2414a57312fc86414ff285e1
white0221 <whiterm.r0221@gmail.com>
2018-12-28 16:03:39 +0900

    Setting User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save { self.email = email.downcase }
+  validates :user_name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 end
