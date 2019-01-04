80d0f1638b0b7ccebe365e81075016e2f1d45e84
white0221 <whiterm.r0221@gmail.com>
2019-01-05 00:54:31 +0900

    Add ユーザ登録時の成功メッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "ユーザを登録しました。"
       redirect_to '/'
     else
       flash[:danger] = "入力エラーです。"
