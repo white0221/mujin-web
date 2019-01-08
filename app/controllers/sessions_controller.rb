a25ad90f00d3e236fd6bc97cc46c928d9d9fb5ae
white0221 <whiterm.r0221@gmail.com>
2019-01-08 16:14:15 +0900

    Create SignIn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,17 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # logedin
+      flash[:success] = "Welcome to MUJIN website"
+      sign_in user
+      redirect_to '/user/list'
+    else
+      # login err
+      flash[:danger] = "Invaid email or password"
+      redirect_to '/signin'
+    end
   end
 
   def destroy
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+  # セッションの発行
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+
+  # ログイン中のユーザを返す
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  # ログインしていたらTrue, その他ならFalse
+  def signed_in?
+    !current_user.nil?
+  end
 end
