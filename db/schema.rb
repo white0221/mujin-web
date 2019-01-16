449461911147774b5f264eb3b8d2ab54e354d618
AltTether <200351p@ugs.kochi-tech.ac.jp>
2019-01-16 18:11:05 +0900

    historiesモデルのスキーマを削除

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_01_16_085612) do
 
-  create_table "histories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.integer "quantity"
-    t.datetime "datetime"
-    t.boolean "response_flag"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_histories_on_user_id"
-  end
-
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.integer "value"
@@ -38,5 +28,4 @@ ActiveRecord::Schema.define(version: 2019_01_16_085612) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "histories", "users"
 end
