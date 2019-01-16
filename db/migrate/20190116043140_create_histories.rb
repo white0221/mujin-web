f51b53d0c30eef00d8108a69c7a57f8853e8aa75
AltTether <200351p@ugs.kochi-tech.ac.jp>
2019-01-16 13:47:42 +0900

    historyモデルのuser_idカラムがuserモデルを参照するように変更

--- db/migrate/20190116043140_create_histories.rb
+++ db/migrate/20190116043140_create_histories.rb
@@ -2,7 +2,7 @@ class CreateHistories < ActiveRecord::Migration[5.2]
   def change
     create_table :histories do |t|
       t.integer :id
-      t.integer :user_id
+      t.references :user, foreign_key: true
       t.integer :quantity
       t.datetime :date
       t.boolean :response_flag
