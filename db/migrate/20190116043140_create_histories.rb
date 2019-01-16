a6464c1c47bc9e8cb82ec4c324fb81af1681d192
AltTether <200351p@ugs.kochi-tech.ac.jp>
2019-01-16 14:09:56 +0900

    historyモデルのidカラムを削除

--- db/migrate/20190116043140_create_histories.rb
+++ db/migrate/20190116043140_create_histories.rb
@@ -1,7 +1,6 @@
 class CreateHistories < ActiveRecord::Migration[5.2]
   def change
     create_table :histories do |t|
-      t.integer :id
       t.references :user, foreign_key: true
       t.integer :quantity
       t.datetime :datetime
