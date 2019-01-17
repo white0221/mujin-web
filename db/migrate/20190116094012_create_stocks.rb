3e1f245164448c89de0f6dbe4037fe55229140b1
AltTether <200351p@ugs.kochi-tech.ac.jp>
2019-01-17 22:28:13 +0900

    stockにitemの外部キー制約を追加

--- db/migrate/20190116094012_create_stocks.rb
+++ db/migrate/20190116094012_create_stocks.rb
@@ -1,6 +1,7 @@
 class CreateStocks < ActiveRecord::Migration[5.2]
   def change
     create_table :stocks do |t|
+      t.references :item, foreign_key: true
       t.integer :quantity
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_085612) do
+ActiveRecord::Schema.define(version: 2019_01_16_094012) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.integer "value"
+    t.string "name", null: false
+    t.integer "value", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "stocks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "item_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_stocks_on_item_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -28,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_01_16_085612) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "stocks", "items"
 end
